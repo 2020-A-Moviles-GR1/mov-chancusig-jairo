0962cb20e8f11bbae2bd59efb188f19b45780b0c
Jairo Chancusig <jairocka00hotmail.com>
2020-07-06 09:55:54 -0500

    New commit

---
+++ 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.moviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("Activity","OnCreate")
+    }
+
+    override fun onStart{
+        super.onStart()
+        Log.i("Activity","onStart")
+    }
+    override fun onResume{
+        super.onResume()
+        Log.i("Activity","onResume")
+    }
+    override fun onPause{
+        super.onPause()
+        Log.i("Activity","onPause")
+    }
+    override fun onStop{
+        super.onStop()
+        Log.i("Activity","onStop")
+    }
+    override fun onDestroy{
+        super.onDestroy()
+        Log.i("Activity","onDestroy")
+    }
+}
